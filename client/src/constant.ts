0aa9bc20b16a9c033609d3a86a13b4a5279a8629
Akshat Garg <akshatarungarg78@gmail.com>
2021-02-06 01:57:21 +0530

    dev server url updated

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,6 +1,6 @@
 export const URLS = {
   DEV : "http://localhost:5000",
-  PROD : "https://xmeme-crio.herokuapp.com/memes",
+  PROD : "https://xmeme-crio.herokuapp.com",
 }
 export interface memeData{ 
   posted_by: string,

947a22ae2a94ad60917e05413fbd999e2dd441b2
Akshat Garg <akshatarungarg78@gmail.com>
2021-02-09 15:27:30 +0530

    install file update

--- install.sh
+++ install.sh
@@ -2,9 +2,19 @@
 
 
 # Any installation related commands
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
-sudo apt-get install -y mongodb-org
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt update
+
+sudo apt install -y mongodb-org
+
+
 
 
 # Any configuration related commands
 
+sudo systemctl start mongod
+
+sudo systemctl enable mongod

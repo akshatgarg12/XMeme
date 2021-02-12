6efb7e477702cf6d6872d7b2398858bcce54fb61
Akshat Garg <akshatarungarg78@gmail.com>
2021-02-12 21:39:37 +0530

    api button on frontend

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -4,6 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <h1>XMeme</h1>
+      <h4><a href= "https://xmeme-crio.herokuapp.com/swagger-ui/">API</a></h4>
     </header>
   );
 }

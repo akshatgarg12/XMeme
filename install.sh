f05a94e986e48a6ced3079dad2078e67b6b25d3f
Akshat Garg <akshatarungarg78@gmail.com>
2021-02-19 03:09:26 +0530

    shell file changes

--- install.sh
+++ install.sh
@@ -6,9 +6,21 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
+# update local machine
 sudo apt update
 
+# install mongodb packages
 sudo apt install -y mongodb-org
+
+#start MongoDB
+sudo systemctl start mongod
+
+# display MongoDB running status
+sudo systemctl status mongod
+
+# make mongodb as a service and restart on reboots
+sudo systemctl enable mongod
+
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
@@ -19,15 +31,5 @@ nvm install node
 node -v
 npm -v
 
-sudo apt update -y
-sudo apt install git -y
-git -v
-
 # Any configuration related commands
 
-
-sudo systemctl start mongod
-
-sudo systemctl enable mongod
-
-mongo XMeme --eval "db.dropDatabase()"
--- server_run.sh
+++ server_run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-
 cd server
-
 
 # Setup DB or any other environment variables you want to setup.
 
+# activate nvm
+. ~/.nvm/nvm.sh
 
 npm install
 
-npm start
+npm run start

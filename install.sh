1746fdf05dbde1033aab47ccef65aaba1b0efea2
Akshat Garg <akshatarungarg78@gmail.com>
2021-02-19 03:44:10 +0530

    shell file changes

--- install.sh
+++ install.sh
@@ -24,10 +24,9 @@ sudo systemctl enable mongod
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
-
-nvm install node
-
+# . ~/.nvm/nvm.sh
+sudo apt-get install npm
+# nvm install node
 node -v
 npm -v
 
--- server_run.sh
+++ server_run.sh
@@ -4,7 +4,7 @@ cd server
 # Setup DB or any other environment variables you want to setup.
 
 # activate nvm
-. ~/.nvm/nvm.sh
+# . ~/.nvm/nvm.sh
 
 npm install
 
